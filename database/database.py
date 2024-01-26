c7a585690d6e45494ef003555c47e464cf6070d1
FaheemBEG <faheem.beg@hotmail.fr>
2024-01-26 17:34:39 +0100

    merge with dev branch

--- database/database.py
+++ database/database.py
@@ -1,54 +1,85 @@
-import sqlite3
 import pandas as pd
 import os
-import pandas as pd
+import wget
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from datascrapping import *
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 path=ROOT_DIR+"/_data"
 
-def create_database():
-    # Connexion à la base de données SQLite
-    conn = sqlite3.connect(f'{path}/WebScrapping.db')
+def create_data_files(path=path,bool_scrap_howlongtobeat:bool=False,bool_scrap_canyourunit:bool=False):  # False by default because data already scraped in /_data
+    print(f"\n Creating data files . . .")
 
-    create_table_queries=["CREATE TABLE IF NOT EXISTS processors (Model TEXT PRIMARY KEY,'TDP (W)' REAL);",
-                          "CREATE TABLE IF NOT EXISTS gpu (Model TEXT PRIMARY KEY,'TDP (Watts)' REAL);",
-                          "CREATE TABLE IF NOT EXISTS games (Title TEXT PRIMARY KEY,'Main Story (Hours)' REAL,Platforms TEXT,Genres TEXT,Images TEXT,CPU TEXT,GPU TEXT,RAM REAL);",
-                          "CREATE TABLE IF NOT EXISTS boavizta_data ( manufacturer TEXT, name TEXT PRIMARY KEY, category TEXT, subcategory TEXT, gwp_total INTEGER, gwp_use_ratio REAL, yearly_tec INTEGER, lifetime INTEGER, use_location TEXT, report_date TEXT, sources TEXT, sources_hash TEXT, gwp_error_ratio REAL, gwp_manufacturing_ratio REAL, weight REAL, assembly_location TEXT, screen_size INTEGER, server_type TEXT, hard_drive TEXT, memory TEXT, number_cpu INTEGER, height REAL, added_date TEXT, add_method TEXT, gwp_transport_ratio REAL, gwp_eol_ratio REAL, gwp_electronics_ratio REAL, gwp_battery_ratio REAL, gwp_hdd_ratio REAL, gwp_ssd_ratio REAL, gwp_othercomponents_ratio REAL, comment TEXT );"
-                          ]
-    
+    scrap_processors()
+    scrap_graphiccards()
+
+    while bool_scrap_howlongtobeat:
+        try:
+            scrap_howlongtobbeat()
+        except Exception as e:
+            print(e)
+            pass
+
+    if bool_scrap_canyourunit:
+        # Scrapping Can You Run It website by avoiding games that are already scrapped :
+        games_scrapped=get_games_scrapped_number()
+        while "9" not in games_scrapped.keys():
+            try:
+                print("\nGames already scrapped : ",games_scrapped)
+                scrap_canyourunit(games_scrapped=games_scrapped)
+                games_scrapped=get_games_scrapped_number()
+            except Exception as e:
+                games_scrapped=get_games_scrapped_number()
+                print(e)
+                continue
     try:
-        for query in create_table_queries:
-            conn.execute(query)
-    except sqlite3.Error as e:
-        print(f"Erreur lors de la création de la table : {e}")
+        games_part_1 = pd.read_csv(f"{path}/games_part_1.csv")
+        games_part_2 = pd.read_csv(f"{path}/games_part_2.csv")
+    except Exception:
+        print(e)
+        pass
+
+    try:
+
+        games_part_1['Title'] = games_part_1['Title'].str.lower()
+        games_part_2['Title'] = games_part_2['Title'].str.lower()
+
+        # Merging games data files
+        final_df = pd.merge(games_part_2, games_part_1, on='Title', how='outer', suffixes=('', '_part_2'))
+        final_df['Image'] = final_df['Image'].combine_first(final_df['Image_part_2'])
+
+        # Deleting duplicated columns
+        final_df.drop(['Image_part_2'], axis=1, inplace=True)
+
+        final_df['Title'] = final_df['Title'].str.capitalize()
+
+        final_df.to_csv(f"{path}/games.csv",index=False)
+        print(f"\nComplete games data file successfuly created !")
+
+    except Exception as e:
+        print(e)
+        pass
     
-    # Fermer la connexion à la base de données
-    return conn
+    print(f"\n>>> Downloading Boavizta data file")
+    if os.path.exists(f"{path}/boavizta_data.csv"):
+        os.remove(f"{path}/boavizta_data.csv")
 
-def connect_to_database():
-    # Vérifier si la base de données existe
-    db_path = f"{path}/WebScrapping.db"
-
-    db_exists = os.path.exists(db_path)
-
-    # Connexion à la base de données SQLite
-    conn = sqlite3.connect(db_path)
-
-    if not db_exists:
-        # Si la base de données n'existe pas, créer la base de données
-        conn = create_database()
-    else:
-        conn = sqlite3.connect(db_path)
-    
-    print(f"\nSuccessfuly connected to dabase : {db_path}")
-
-    return conn
-
+    wget.download("https://raw.githubusercontent.com/Boavizta/environmental-footprint-data/main/boavizta-data-us.csv",f"{path}/boavizta_data.csv")
+    print(f"\nData scrapped and data files successfuly created in : {path}")
 
 def clean_games_dataframe(dataframe):
 
     dataframe = dataframe.astype('string')
     dataframe["Main Story (Hours)"] = pd.to_numeric(dataframe["Main Story (Hours)"].str.extract('(\d+)', expand=False), errors='coerce')
+    dataframe.fillna(value={'Main Story (Hours)':-1,'CPU':"",'GPU':"",'Platforms':"",'Genres':"",'RAM':"",'Image':""},inplace=True)
 
     return dataframe
 
+def csv_to_json(file_path:str=path):
+    return
+
+if __name__ == "__main__":
+    create_data_files()
+    csv_to_json()
